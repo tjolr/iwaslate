98d709024964b16d8e207990727c02bd950a9af0
Tjøl Ravndal <tjol.ravndal@gmail.com>
2022-02-24 16:02:49 +0100

    save latecomings

--- src/app.tsx
+++ src/app.tsx
@@ -2,13 +2,21 @@ import { Logo } from "./logo";
 import Hero from "./components/Hero";
 import theme from "./theme/theme";
 import { ThemeProvider } from "styled-components";
-
+import { SnackbarProvider } from "notistack";
 
 export const App = () => {
   return (
     <>
       <ThemeProvider theme={theme}>
-        <Hero />
+        <SnackbarProvider
+          maxSnack={3}
+          anchorOrigin={{
+            vertical: "top",
+            horizontal: "center",
+          }}
+        >
+          <Hero />
+        </SnackbarProvider>
       </ThemeProvider>
     </>
   );
