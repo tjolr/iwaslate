82f97d060a4ff4d33623c0cd7bc304319ee87c42
Anders Solberg <ssredna@gmail.com>
2022-02-25 20:08:53 +0100

    correctly configure .env, and remove preact logo

--- src/supabase/supabase.ts
+++ src/supabase/supabase.ts
@@ -1,8 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://sdyyceitwjnhrmxogftf.supabase.co";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
 
-//const supabaseAnonKey = process.env.SUPABASE_ANON_KEY as string;
-const supabaseAnonKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNkeXljZWl0d2puaHJteG9nZnRmIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDU2MzEzMzAsImV4cCI6MTk2MTIwNzMzMH0.lvunm5kdgimqd4_Q1ZSwCTUYRkz06l13nT10_Joj4fc";
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
